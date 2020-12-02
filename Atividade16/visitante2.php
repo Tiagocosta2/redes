738af6866e90d3bd52df0915ab601dfcaf45fd14
TIAGO <TIAGO@ASUS.local>
2020-12-02 09:53:57 +0000

    Atividade16

---
+++ Atividade16/visitante2.php
@@ -0,0 +1,23 @@
+<?php
+echo "<span style='color:blue;'><h3><b>****************************Pagina Visitante 2*********************</b></h3></span><br>";
+if (! isset($_COOKIE['nome']) && !isset($_COOKIE['idade'])) {
+	echo "<script>alert('As COOKIES passaram de validade (expiraram).\\n\n\\Elas deixaram de existir!');</script>";
+	exit('<script>location.href = "Formulario.html"</script>');
+}
+else {
+
+$nome2 = $_COOKIE['nome'];
+$idade2 = $_COOKIE['idade'];
+
+echo "Nome: ".$_COOKIE['nome']."<br>";
+echo "Idade: ".$_COOKIE['idade'];
+
+echo "<br><br>";
+echo "Nome: ".$nome2 ."<br>";
+echo "Idade: ".$idade2;
+
+echo "<a href='Formulario.html'><h5><b>Voltar</b></h5></a> ";
+echo "<a href='Principal.php'><h5><b>Principal</b></h5></a> ";
+echo "<a href='Visitante1.php'><h5><b>Visitante 1</b></h5></a> ";
+}
+?>
